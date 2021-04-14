bd768863f14e10759aa12b2ce4bda5c65b79909d
Vitor Lourenco <vitor.registros@lourencos.net>
2021-04-14 19:06:15 -0300

    Add login and send message functionalities

--- scrips/main.js
+++ scrips/main.js
@@ -6,15 +6,42 @@ const overlay = document.querySelector('.overlay');
 const roomContainer = document.querySelector('.room-container');
 const messageSummary = document.querySelector('.message-summary');
 const messageInput = document.querySelector('.new-message-container input');
+const chatBody = document.querySelector('.chat-body');
 let target = 'Todos';
-let privacy = 'Publico';
+let privacy = 'message';
 let username;
 
 function login(){
   loginStatic.classList.add('d-none');
   loginLoading.classList.remove('d-none');
-  // do some request here
-  loginScreen.classList.add('d-none');
+  username = document.querySelector('.static INPUT').value;
+  axios
+    .post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants', {name: username})
+    .then(()=>{
+      loginScreen.classList.add('d-none');
+      updateActiveUsers();
+      updateMessages();
+      setInterval(updateActiveUsers, 10000); 
+      setInterval(updateMessages, 3000);
+    })
+    .catch();
+}
+
+function resetMessageText(){
+  messageInput.value = '';
+}
+
+function sendMessage(){
+  currentMessage = messageInput.value;
+  axios
+    .post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages'
+      , {from: username, to: target, text: currentMessage, type: privacy})
+    .then(()=>{
+      resetMessageText();
+    })
+    .catch(()=>{
+      alert('mensagem nao enviada');
+    });
 }
 
 loginInput.addEventListener('keydown', (e)=>{
@@ -22,9 +49,7 @@ loginInput.addEventListener('keydown', (e)=>{
 });
 
 messageInput.addEventListener('keydown', (e)=>{
-  if (e.key === 'Enter'){
-    console.log('enter has been pressed');
-  }
+  if (e.key === 'Enter') sendMessage();
 });
 
 function showRoom(){
@@ -33,8 +58,10 @@ function showRoom(){
 }
 
 function hideRoom(){
-  overlay.classList.add('d-none');
   roomContainer.classList.add('room-hidden');
+  setTimeout(()=>{
+    overlay.classList.add('d-none')
+  }, 300);
 }
 
 function selectUnique(domElem){
@@ -47,7 +74,8 @@ function selectUnique(domElem){
 }
 
 function updateSummary(){
-  messageSummary.textContent = `Enviando para ${target} (${privacy})`;
+  const privacyText = privacy === 'mensagem' ? 'Publico' : 'Reservadamente';
+  messageSummary.textContent = `Enviando para ${target} (${privacyText})`;
 };
 
 function setTarget(domElem){
@@ -58,7 +86,11 @@ function setTarget(domElem){
 
 function setPrivacy(domElem){
   selectUnique(domElem);
-  privacy = domElem.querySelector('.privacy-value').textContent;
+  if (domElem.querySelector('.privacy-value').textContent === 'Publico'){
+    privacy = 'message';
+  } else {
+    privacy = 'private_message';
+  }
   updateSummary();
 }
 
@@ -66,5 +98,24 @@ function updateActiveUsers(){
   
 }
 
-updateActiveUsers();
-setInterval(updateActiveUsers, 10000); 
+function formatMessage(obj){
+  const formatedMessage = `
+    <div class="message bg-dm">
+      <span class="timestamp">(09:21:25)</span>&nbsp;
+      <span class="sender">Joao</span>&nbsp;
+      reservadamente para&nbsp;
+      <span class="target">Maria</span>:&nbsp;
+      Oi gatinha quer tc?
+    </div>`
+  return formatedMessage;
+}
+
+function updateMessages(){
+  axios
+    .get('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages')
+    .then(({data})=>{
+      chatBody.innerHTML = '';
+      data.forEach(elem=>{chatBody.innerHTML += formatMessage(elem)});
+    })
+    .catch();
+}
